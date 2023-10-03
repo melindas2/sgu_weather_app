9a6090d62374cc9fdb4fb0f0a62075c2b23ebecc
melindas2 <melinda.santoso@student.sgu.ac.id>
2023-10-03 15:31:00 +0700

    Chire : Initial commit

---
+++ src/App.js
@@ -0,0 +1,60 @@
+import logo from './logo.svg';
+import './App.css';
+import Button from 'react-bootstrap/Button';
+
+function App() {
+  return (
+    <div class="container">
+        <h1>Weather App</h1>
+        <table class="centerContainer1">
+            <tr>
+                <td>
+                <label id="cityLabel" for="cityNameTextField" style={{color: "black"}}>Enter a city:</label>
+                </td>
+            </tr>
+            <tr>
+                <td>
+                    <input id="cityNameTextField"type="text" class="form-control" name="cityName"/>
+                    <button id="submitbutton" class="tombol btn btn-primary btn-sm" value="Search" onclick="submitAction()">Search</button>
+                </td>
+            </tr>
+        </table>
+        <div class="spacer"></div>
+        <div
+                id="errorMessageComponent"
+                style={{display: 'none', color: "#fc574e"}}
+        ></div>
+        <div>
+        <div id="loader" style={{display: 'none'}} class="spinner spinner-border" role="status">
+            <span class="sr-only">Loading...</span>
+        </div>
+        </div>
+        <div id="weatherInfoComponent" style={{display: "none"}}>
+        <table class="centerContainer2 table">
+            <tr>
+                <th>Weather</th> 
+                <td id = "weatherField" class="td1">0</td>
+            </tr>
+            <tr>
+                <th>Temperature</th> 
+                <td id = "temperatureField" class="td1">0</td>
+            </tr>
+            <tr>
+                <th>Feels like</th>
+                <td id="feelsLikeField" class="td1">0</td>
+            </tr>
+            <tr>
+                <th>Humidity</th>
+                <td id="humidityField" class="td1">0</td>
+            </tr>
+            <tr>
+                <th>Atmospheric Pressure</th>
+                <td id="pressureField" class="td1">0</td>
+            </tr>
+        </table>
+        </div>
+    </div>
+  );
+}
+
+export default App;
